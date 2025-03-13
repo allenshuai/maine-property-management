53e7428b4607c419a94fd3079605ccf6694f52b5
allenshuai <zhufang.wang@tufts.edu>
2025-03-13 00:30:49 -0400

    launching page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import {Monoton} from "next/font/google";
 
+const monotonFont = Monoton({
+  subsets: ["latin"],
+  weight: "400",
+
+})
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
-      <h1 className="text-5xl font-bold">Main Property Management</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+      <h1 className={`text-6xl mb-6 font-bold ${monotonFont.className}`}>Main Property Management.</h1>
+      <h2 className="text-4xl font-bold">COMING SOON.</h2>
     </div>
   );
 }
