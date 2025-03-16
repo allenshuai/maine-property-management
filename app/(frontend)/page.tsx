db64ac774d8f3804d3d2ec938dca764e64e08d64
allenshuai <zhufang.wang@tufts.edu>
2025-03-15 20:15:14 -0400

    bug

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,12 +1,12 @@
-import {Monoton} from "next/font/google";
+// import {Monoton} from "next/font/google";
 import HomeHeroSection from "./components/HomeHeroSection";
 import OfferSection from "./components/HomeOfferSection";
 
-const monotonFont = Monoton({
-  subsets: ["latin"],
-  weight: "400",
+// const monotonFont = Monoton({
+//   subsets: ["latin"],
+//   weight: "400",
+// })
 
-})
 export default function Home() {
   return (
     <main className="">
