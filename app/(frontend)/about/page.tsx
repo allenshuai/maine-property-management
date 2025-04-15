ab1f875114210a70a0c76b50fd68cc1094088683
allenshuai <zhufang.wang@tufts.edu>
2025-04-15 01:01:18 -0400

    add again

---
+++ app/(frontend)/about/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+
+export default function AbouttPage() {
+    return (
+      <div className="md:mt-8 flex md:flex-row flex-col min-h-screen px-6 md:px-16 py-12 max-w-screen-xl mx-auto">
+        <div className="flex flex-col md:flex-row justify-between md:items-start mb-12 text-gray-700">
+          <h1 className="text-3xl md:text-5xl font-normal md:w-1/2 flex-1">About Us.</h1>
+          
+        </div>
+  
+        {/* <div className="flex justify-center">
+          <ContactForm />
+        </div> */}
+      </div>
+    );
+  }
